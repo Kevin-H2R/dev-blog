bf3da143e906c96af7899555b5663f14cbd67e1d
Kevin Herrbach <herrbach.kevin@gmail.com>
2023-09-21 22:10:38 +0900

    Export nav bar in own component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Kebinou's dev blog</h1>
+      </div>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
