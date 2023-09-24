1b57ade779a11f7949d8ac7e13b9c962aa4ce3a4
Kevin <herrbach.kevin@gmail.com>
2023-09-24 23:34:50 +0900

    Adds logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="d-flex align-center justify-space-between app-bar">
-    <div>
-      <h1>Kebinou's dev blog</h1>
-    </div>
+    <img src="@/assets/logo.jpeg" class="logo" />
     <div class="categories d-flex flex-grow-1 justify-center">
       <router-link class="category" to="/">All</router-link>
       <router-link class="category" to="/leetcode">Leetcode</router-link>
       <router-link class="category" to="/random">Random</router-link>
     </div>
-    <v-spacer></v-spacer>
   </div>
 </template>
 <script>
@@ -21,9 +18,13 @@ export default {
 .app-bar {
   border-bottom: 1px solid $background-darken;
   background-color: $background !important;
+  height: 32px;
 }
 .categories {
   gap: 10px;
+}
+.logo {
+  height: 32px;
 }
 .category {
   cursor: pointer;
