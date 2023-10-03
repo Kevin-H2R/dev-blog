305ff2976cb58bfc613a88f3c1b4d9bdca3316cb
Kevin Herrbach <herrbach.kevin@gmail.com>
2023-10-03 20:23:49 +0900

    Changes bucket url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <v-col>
     <v-row class="main">
-      <div class="main__title">
-        My engineer journal.
-      </div>
-      <div class="main__subtitle">
-        I am Kevin, French software engineer. Currently in South Korea, always looking for new smart ideas and business.
-      </div>
-      <div class="main__caption">
-        Coding problems, architecture solutions, anything I feel relevent sharing.
-      </div>
+      <v-col cols="12" sm="10" md="8">
+        <div class="main__title">
+          My engineer journal.
+        </div>
+        <div class="main__subtitle">
+          I am Kevin, French software engineer. Currently in South Korea, always looking for new smart ideas and business.
+        </div>
+        <div class="main__caption">
+          Coding problems, architecture solutions, anything I feel relevent sharing.
+        </div>
+      </v-col>
     </v-row>
     <div class="home d-flex justify-center">
       <v-col cols="12" sm="5" md="3">
