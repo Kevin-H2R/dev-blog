7ea79566d1d55efc5c39cab06b4d1057a9b8c48e
Kevin <herrbach.kevin@gmail.com>
2023-11-22 13:41:46 +0900

    Adds gtag package

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueGtag from 'vue-gtag'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueGtag, {
+  id: 'G-5FK8YSJ92Z'
+}, router)
 
 new Vue({
   vuetify,
